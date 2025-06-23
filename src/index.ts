617e964a71226fdbb72ca2356fc24c6a71fe894d
r0h0gg6 <richard.heng0@walmart.com>
2025-06-23 10:56:03 -0700

    Update software components release for agp 8.7.2

--- src/index.ts
+++ src/index.ts
@@ -86,7 +86,7 @@ publishing {
                 groupId = "${extra.groupId}"
                 artifactId = "${extra.artifactId}"
                 version = "${containerVersion}"
-                from components.release
+                from components["release"]
                 artifact tasks.androidSourcesJar
             }
         }
