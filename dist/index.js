93473f5f8cf51fd307ffcfaf4a5a4d9aebc57f74
r0h0gg6 <richard.heng0@walmart.com>
2025-07-09 01:28:07 -0700

    test

--- dist/index.js
+++ dist/index.js
@@ -69,6 +69,9 @@ publishing {
                     from components.release
                 } else if (components.findByName('default')) {
                     from components.default
+                } else {
+                    // Fallback for manual configuration
+                    artifact bundleReleaseAar
                 }
                 artifact tasks.androidSourcesJar
             }
